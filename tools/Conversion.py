3e4fcba3c1d25d992b56805c5762b3607c92d751
PhilipPhil <philipgarabandic@gmail.com>
2021-06-24 16:05:38 +0100

    convs nice

--- tools/Conversion.py
+++ tools/Conversion.py
@@ -1,17 +1,40 @@
 import numpy as np
 
 class Conversion:
+
+    VOLTAGE_CHANNEL = 0
+    CURRENT_CHANNEL = 1
+
+    # rough assumption: 325 (V pp) covers ~65% of the dynamic range (15 bits because we have 16-bit unsigned int)
+    # hence 0.65 * 2 ** 15
+    DEFAULT_VOLTAGE_SCALING = 325 / (0.65 * 2 ** 15)
+
+    # rough assumption: 18 (A pp) covers ~70% of the dynamic range (15 bits because we have 16-bit unsigned int)
+    # hence 0.7 * 2 ** 15
+    DEFAULT_CURRENT_SCALING = 18 / (0.7 * 2 ** 15)
+
+    DEFAULT_SAMPLE_RATE = 10000  # Hz
+    DEFAULT_MAINS_FREQ = 50  # Hz
     
-    def __init__(self, voltage: np.array, current: np.array):
-        # Fundental Frequency is 50Hz
-        # 10000Hz / 50Hz = 200
-        self.__voltage = voltage.reshape((1500, 200))
-        self.__current = current.reshape((1500, 200))
+    def __init__(self, data: np.array):
+        self.__voltage, self.__current = self.__prepare_date(data)
         
         self.real_power = self.__realPower()
         self.apparent_power = self.__apparentPower()
         self.reactive_power = self.__reactivePower()
         self.thd = self.__totalHarmonicDistortion()
+
+    def __prepare_date(self, data: np.array):
+        recentered = data - data.mean(axis=0)
+        recentered[:, self.VOLTAGE_CHANNEL] *= self.DEFAULT_VOLTAGE_SCALING
+        recentered[:, self.CURRENT_CHANNEL] *= self.DEFAULT_CURRENT_SCALING
+        self.voltage, self.current = recentered[:, self.VOLTAGE_CHANNEL], recentered[:, self.CURRENT_CHANNEL]
+
+        # Fundental Frequency is 50Hz
+        # 10000Hz / 50Hz = 200
+        voltage = self.voltage.reshape((1500, 200))
+        current = self.current.reshape((1500, 200))
+        return voltage, current
     
     def __realPower(self):
         real_power = (self.__voltage * self.__current).mean(axis = 1)
