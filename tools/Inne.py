8bb5ea0db92264e77e87ebd77ed857acda5aee58
PhilipPhil <philipgarabandic@gmail.com>
2021-06-30 00:58:37 +0100

    Inne

---
+++ tools/Inne.py
@@ -0,0 +1,9 @@
+from tools.Cluster import Cluster
+import pandas as pd
+from anomatools.models import iNNE
+
+class Inne(Cluster):    
+    def cluster(self, x_row: pd.DataFrame) -> list:
+        detector = iNNE().fit(x_row)
+        anomaly_score = detector.predict_proba(x_row, method='unify')[:,1]
+        return anomaly_score.tolist()
