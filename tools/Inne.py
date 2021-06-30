a9c7bad4ed2059932272960d1942a9683ea4cc59
PhilipPhil <philipgarabandic@gmail.com>
2021-06-30 02:28:32 +0100

    no message

--- tools/Inne.py
+++ tools/Inne.py
@@ -2,8 +2,10 @@ from tools.Cluster import Cluster
 import pandas as pd
 from anomatools.models import iNNE
 
-class Inne(Cluster):    
+class Inne(Cluster):
+    n_members = 200
+    sample_size = 32
     def cluster(self, x_row: pd.DataFrame) -> list:
-        detector = iNNE().fit(x_row)
+        detector = iNNE(n_members=self.n_members, sample_size=self.sample_size).fit(x_row)
         anomaly_score = detector.predict_proba(x_row, method='linear')[:,1]
         return anomaly_score.tolist()
