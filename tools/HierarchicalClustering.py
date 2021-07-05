8570a59092f76c3c5308e8df2af7574b183abce2
PhilipPhil <philipgarabandic@gmail.com>
2021-07-05 16:36:39 +0100

    no message

--- tools/HierarchicalClustering.py
+++ tools/HierarchicalClustering.py
@@ -1,15 +1,16 @@
 from tools.Cluster import Cluster
 import pandas as pd
+import numpy as np
 from collections import Counter
 from sklearn.cluster import AgglomerativeClustering
 
 class HierarchicalClustering(Cluster):
     affinity='euclidean'
-    linkage='ward'
-    distance_threshold=15
+    linkage='average'
+    distance_threshold = 18.93 # Known from expert knowlege
     
-    def cluster(self, x_row: pd.DataFrame) -> list:   
-        clustering = AgglomerativeClustering(n_clusters=None, affinity='euclidean', linkage='ward', distance_threshold=self.distance_threshold).fit(x_row)
+    def cluster(self, x_row: pd.DataFrame) -> list:
+        clustering = AgglomerativeClustering(n_clusters=None, affinity=self.affinity, linkage=self.linkage, distance_threshold=self.distance_threshold).fit(x_row)
         labels = clustering.labels_
         color_counts = Counter(labels)
         anomaly_score = [1 - color_counts[color] / len(labels) for color in labels]
