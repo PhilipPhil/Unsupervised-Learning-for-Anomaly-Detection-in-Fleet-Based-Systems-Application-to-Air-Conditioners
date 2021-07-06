85c6f2d1b3f1b7128dbc327a3877926fda1fb611
PhilipPhil <philipgarabandic@gmail.com>
2021-07-06 14:11:58 +0100

    no message

--- tools/KNN.py
+++ tools/KNN.py
@@ -7,8 +7,8 @@ class KNN(Cluster):
 
     def cluster(self, x_row: pd.DataFrame) -> list:
         x_row = (x_row - x_row.min()) / (x_row.max() - x_row.min())
-        nbrs = NearestNeighbors(n_neighbors=len(x_row)//2, algorithm='brute', metric=self.metric).fit(x_row)
+        nbrs = NearestNeighbors(n_neighbors=len(x_row)//2+1, algorithm='brute', metric=self.metric).fit(x_row)
         distances, indices = nbrs.kneighbors(x_row)
-        labbels = distances.mean(axis=1)
+        labbels = distances.sum(axis=1)
         anomaly_score = (labbels - min(labbels)) / (max(labbels) - min(labbels))
         return anomaly_score.tolist()
