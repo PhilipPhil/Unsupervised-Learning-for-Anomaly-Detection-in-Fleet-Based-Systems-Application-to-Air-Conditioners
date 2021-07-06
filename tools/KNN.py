1d0558138d42bfde28923036b8c9be184bcb0a8f
PhilipPhil <philipgarabandic@gmail.com>
2021-07-06 12:54:06 +0100

    no message

--- tools/KNN.py
+++ tools/KNN.py
@@ -3,8 +3,11 @@ import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
 class KNN(Cluster):
+    metric ='euclidean'
+
     def cluster(self, x_row: pd.DataFrame) -> list:
-        nbrs = NearestNeighbors(n_neighbors=len(x_row)//2, algorithm='brute', metric='manhattan').fit(x_row)
+        x_row = (x_row - x_row.min()) / (x_row.max() - x_row.min())
+        nbrs = NearestNeighbors(n_neighbors=len(x_row)//2, algorithm='brute', metric=self.metric).fit(x_row)
         distances, indices = nbrs.kneighbors(x_row)
         labbels = distances.mean(axis=1)
         anomaly_score = (labbels - min(labbels)) / (max(labbels) - min(labbels))
--- tools/LOF.py
+++ tools/LOF.py
@@ -3,8 +3,11 @@ import pandas as pd
 from sklearn.neighbors import LocalOutlierFactor
 
 class LOF(Cluster):
+    metric ='euclidean'
+
     def cluster(self, x_row: pd.DataFrame) -> list:
-        clf = LocalOutlierFactor(n_neighbors=len(x_row)//2, algorithm='brute', metric='manhattan').fit(x_row)
-        labels = clf.negative_outlier_factor_
-        anomaly_score = 1 - (labels - min(labels))/(max(labels) - min(labels))
+        x_row = (x_row - x_row.min()) / (x_row.max() - x_row.min())
+        clf = LocalOutlierFactor(n_neighbors=len(x_row)//2, algorithm='brute', metric=self.metric).fit(x_row)
+        labels = clf.negative_outlier_factor_ * (-1)
+        anomaly_score = (labels - min(labels))/(max(labels) - min(labels))
         return anomaly_score.tolist()
